2e847636bec33280d0586cbca9c85b2977084145
João Emanuel <joaoemanueljanauba@gmailc.com>
2025-02-11 17:54:56 -0300

    colocando para pt-br

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
 };
 
 export default nextConfig;
